a844452ad13bed07a8aba87bfb05fdb9e2c4f51c
Agustina <agustinajait@gmail.com>
2026-05-15 21:49:39 -0300

    fix next config for vercel

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    domains: ['oportunai.com'],
+  experimental: {
+    serverComponentsExternalPackages: ['@prisma/client', 'bcryptjs'],
   },
-};
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig

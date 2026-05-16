a4b0c5527da1b7532da5551141fdad0735138d53
Agustina <agustinajait@gmail.com>
2026-05-15 22:32:54 -0300

    cleanup temp files

--- fix-taller.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-const p = 'app/dashboard/grabar-taller/page.tsx';
-let content = fs.readFileSync(p, 'utf8');
-if (!content.includes('force-dynamic')) {
-  fs.writeFileSync(p, "export const dynamic = 'force-dynamic';\n" + content);
-  console.log('Fixed:', p);
-}

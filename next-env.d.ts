4327254c482a7ea8a0c87d5e3164a0a59966bbd4
Agustina <agustinajait@gmail.com>
2026-05-15 21:53:41 -0300

    fix postulantes route dynamic

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,11 +1,5 @@
-﻿/// <reference types="next" />
+/// <reference types="next" />
 /// <reference types="next/image-types/global" />
-/// <reference types="next/navigation-types/compat/navigation" />
 
 // NOTE: This file should not be edited
-// see https://nextjs.org/docs/basic-features/typescript for more information.
-
-declare module '*.css' {
-  const content: { [className: string]: string };
-  export default content;
-}
+// see https://nextjs.org/docs/app/building-your-application/configuring/typescript for more information.
